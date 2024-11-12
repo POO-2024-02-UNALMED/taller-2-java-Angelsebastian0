1ef0af7b56c1932af46ca349337fa4a819fea6db
Angelsebastian0 <ancuaran@unal.edu.co>
2024-11-11 20:00:46 -0500

    Update asiento.java

--- src/test/java/test/asiento.java
+++ src/test/java/test/asiento.java
@@ -1,4 +1,16 @@
-
+package test;
 public class asiento {
+    Integer presio; 
+    Integer registro;
+    String color;
+
+    String cambiarcolor(String color){
+        if(color == "verde" || color == "rojo" || color == "negro" || color == "amarillo" || color == "blanco"){
+            this.color = color;
+        }
+
+            return color;
+        
+    }
 
 }
